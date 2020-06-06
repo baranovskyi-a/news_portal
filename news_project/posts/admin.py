977c3c9503a7b0dde09b3ba0468ccb291ac51f03
Andrii <baranovskyi.and@gmail.com>
2020-06-06 11:43:37 +0300

    impl basic admin funcs

--- news_project/posts/admin.py
+++ news_project/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
+
+
+admin.site.register(Post, PostAdmin)
